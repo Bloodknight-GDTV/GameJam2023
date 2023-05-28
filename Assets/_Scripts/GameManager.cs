29e0d83321c515962d768710783f1b52bbb70cfc
Bloodknight <Bloodknight@users.noreply.github.com>
2023-05-28 13:33:30 +0100

    Create manabar + add hotkey for dimension switch

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,12 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    public ManaBar manaBar;
+    
+    [SerializeField] TMP_Text gameMessagesText;
+    [SerializeField] TMP_Text manaPoolText;
+    
+    // Dimension switching Variables
+    private bool isActiveDimension1;
+    private bool isActiveDimension2;
+        
+    
     
     int manaPool = 0;
+    public int minManaPool = 0;
+    public int maxManaPool = 100;
+    
+    public static int FakeManaPool = 50;
 
     private void Awake()
     {
@@ -15,21 +31,73 @@ public class GameManager : MonoBehaviour
             Destroy(this);
             return;
         }
+        
+        //manaBar.SetMinMana(minManaPool);
 
         Instance = this;
         DontDestroyOnLoad(this);
+        Debug.Log("GameManager Instance; Awake()");
     }
 
-
+    // **************************************
+    //                  Start
+    // **************************************
     private void Start()
     {
+        // Initial dimension setup
+        isActiveDimension1 = true;
+        isActiveDimension1 = false;
         
+        // initial loading of dimensions, this should just load dimension 1
+        // unless of course you derped out and forgot to set the dimensions correctly
+        switchDimensions();
+        
+        // Unload all scenes
+        //SceneManager.UnloadSceneAsync("Dimension01");
+        //SceneManager.UnloadSceneAsync("Dimension02");
+        
+        manaBar.SetMana(manaPool);
+        manaPoolText.text = manaPool.ToString();
+        
+        Debug.Log("GameManager Instance; Start()");
     }
 
-    // Update is called once per frame
+    // **************************************
+    //                  Update
+    // **************************************
     private void Update()
     {
-        
+        manaBar.SetMana(manaPool);
+        manaPoolText.text = manaPool.ToString();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            switchDimensions();
+        }
+
+    }
+    
+    
+    //**********************************************************************************
+    // Custom functions
+    //**********************************************************************************
+    
+    private void switchDimensions()
+    {
+        if (isActiveDimension1)
+        {
+            isActiveDimension1 = false;
+            isActiveDimension2 = true;
+            SceneManager.LoadSceneAsync("Dimension01", LoadSceneMode.Additive);
+            SceneManager.UnloadSceneAsync("Dimension02");
+        }
+        else
+        {
+            isActiveDimension1 = true;
+            isActiveDimension2 = false;
+            SceneManager.LoadSceneAsync("Dimension02", LoadSceneMode.Additive);
+            SceneManager.UnloadSceneAsync("Dimension01");
+        }
     }
     
     // 
--- Assets/_Scripts/LoadStuff.cs
+++ Assets/_Scripts/LoadStuff.cs
@@ -1,16 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+
 
 public class LoadStuff : MonoBehaviour
 {
     // Make Current Scene a Singleton
     private static LoadStuff Instance;
     
-    
-    private bool isActiveDimension1;
-    private bool isActiveDimension2;
+
     
     // For saving and loading objects
     public GameObject prefab;
@@ -30,79 +31,52 @@ public class LoadStuff : MonoBehaviour
     
     private void Start()
     {
-        // Initialise variables as needed
-        isActiveDimension1 = true;
-        isActiveDimension1 = false;
         
-        // Unload all scenes
-        SceneManager.UnloadSceneAsync("Dimension01");
-        SceneManager.UnloadSceneAsync("Dimension02");
         
         // setup for object lists
         goList = new List<GameObject>();
     }
+
     
+    
+    
+    
+
     // Load up a temporary UI for testing
     void OnGUI()
     {
-        if (GUI.Button(new Rect(20, 10, 150, 40), "Switch Dimensions"))
-        {
-            switchDimensions();
-        }
-        
-        if (GUI.Button(new Rect(180, 10, 150, 40), "Reset to Base Scene"))
-        {
-            SceneManager.LoadSceneAsync("PrototypeLevel", LoadSceneMode.Single);
-        }
-    }
-    
-    private void switchDimensions()
-    {
-        if (isActiveDimension1)
-        {
-            isActiveDimension1 = false;
-            isActiveDimension2 = true;
-            SceneManager.LoadSceneAsync("Dimension01", LoadSceneMode.Additive);
-            SceneManager.UnloadSceneAsync("Dimension02");
-        }
-        else
-        {
-            isActiveDimension1 = true;
-            isActiveDimension2 = false;
-            SceneManager.LoadSceneAsync("Dimension02", LoadSceneMode.Additive);
-            SceneManager.UnloadSceneAsync("Dimension01");
-        }
+        // if (GUI.Button(new Rect(20, 10, 150, 40), "Switch Dimensions"))
+        // {
+        //     switchDimensions();
+        // }
+        //
+        // if (GUI.Button(new Rect(180, 10, 150, 40), "Reset to Base Scene"))
+        // {
+        //     SceneManager.LoadSceneAsync("PrototypeLevel", LoadSceneMode.Single);
+        // }
     }
     
     
   
-    /// /////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
 
+    if(Input.GetKeyDown(KeyCode.I)) {
+        // Create a new GameObject from prefab and move to position
+        Vector3 position = new Vector3(0, 0, 0);
+        goList.Add((gameObject));
+        Instantiate(prefab, position, Quaternion.identity);
+    }
     
- 
- 
- 
-    // Inside class --
-
- 
-    /*
-    void Update() {
-        if(Input.GetKeyDown(KeyCode.I) {
-            // This random position is for fun :D
-            Vector3 rndPos = new Vector3(Random.Range(-20, 20), Random.Range(-20, 20), Random.Range(-20, 20));
-     
-            // Create a new GameObject from prefab and move to random position
-            goList.Add((GameObject)Instanciate(prefab, rndPos, Quaternion.Identity);
-        }
- 
-        if(Input.GetKeyDown(KeyCode.L)){
-            Debug.Log(goList.Count);
-            foreach(GameObject go in goList){
-                Debug.Log(go.name);
-            }
+    
+    // List gameObjects in collection
+    if(Input.GetKeyDown(KeyCode.L)){
+        Debug.Log(goList.Count);
+        foreach(GameObject go in goList){
+            Debug.Log(go.name);
         }
     }
-    */
+
+*/
     
     
 }
