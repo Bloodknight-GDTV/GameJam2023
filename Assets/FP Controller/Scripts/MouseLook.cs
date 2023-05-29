cf7393924c6eda30cb22f5a1a4fbf883e84ec765
Bloodknight <Bloodknight@users.noreply.github.com>
2023-05-29 12:39:55 +0100

    Make mouselook work with webGL

--- Assets/FP Controller/Scripts/MouseLook.cs
+++ Assets/FP Controller/Scripts/MouseLook.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
 
-    public float mouseSensitivity = 500f;
+    public float mouseSensitivity = 1500f;
 
     Transform player;
     float xRot;
@@ -18,12 +18,27 @@ public class MouseLook : MonoBehaviour
 
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        float modifier = 1.0f;
+        
+        #if UNITY_WEBGL
+            modifier = 0.05f;
+        #else
+            modifier = 1f;    
+        #endif
+        
+#if UNITY_EDITOR
+        modifier = 1f;
+#endif
+        
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * modifier * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * modifier * Time.deltaTime;
+        
         xRot -= mouseY;
         xRot = Mathf.Clamp(xRot, -90f, 90f);
         transform.localRotation = Quaternion.Euler(xRot, 0, 0);
+
         player.Rotate(Vector3.up * mouseX);
+
     }
 
 }
